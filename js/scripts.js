cb649a26eff03e35e6a7542477b9c421850250b6
Tom Mertz & connor abdelnoor <student@epicodus.com>
2015-04-16 10:38:45 -0700

    Add button that creates new address fields in address book html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,77 @@
+var Address = {
+  fullAddress: function() {
+    return this.street + ", " + this.city + ", " + this.state +  ", " + this.zipcode;
+  }
+};
+
 $(document).ready(function() {
 
   $("#addContact").submit(function(event) {
+    event.preventDefault();
+
     var firstNameForm = $("#firstName").val();
     var lastNameForm = $("#lastName").val();
-    var street = $("#street").val();
-    var city = $("#city").val();
-    var state = $("#state").val();
-    var zipcode = $("#zipcode").val();
-
-    var newAddress = {
-      street: street,
-      city: city,
-      state: state,
-      zipcode: zipcode,
-      fullAddress: function() {
-        return this.street + ", " + this.city + ", " + this.state +  ", " + this.zipcode;
-      }
-    };
-
 
     var contact = {
       firstName: firstNameForm,
       lastName: lastNameForm,
-      address: newAddress.fullAddress()
+      addresses: []
     };
+
+    $(".address").each(function() {
+      var inputStreet = $(this).find("input.street").val();
+      var inputCity = $(this).find("input.city").val();
+      var inputState = $(this).find("input.state").val();
+      var inputZipcode = $(this).find("input.zipcode").val();
+
+      var newAddress = Object.create(Address);
+
+      newAddress.street = inputStreet;
+      newAddress.city = inputCity;
+      newAddress.state = inputState;
+      newAddress.zipcode = inputZipcode;
+
+      contact.addresses.push(newAddress);
+    });
+
+    var street = $(".street").val();
+    var city = $(".city").val();
+    var state = $(".state").val();
+    var zipcode = $(".zipcode").val();
 
     $("#contact-list").append("<li><span class='contact clickable'>" + contact.firstName + "</span></li>");
 
     $(".contact").last().click(function() {
+
       $(".first-name").text(contact.firstName);
       $(".last-name").text(contact.lastName);
-      $(".address").text(contact.address);
+
+
+
       $("#contacts").show();
     });
 
 
-
-    event.preventDefault();
   });
 
+  $('#add-address').click(function(event) {
+    event.preventDefault();
 
+    $('.one-address').first().clone().appendTo($('.addresses'));
+    $("input.street").last().val("");
+    $("input.city").last().val('');
+    $("input.state").last().val('');
+    $("input.zipcode").last().val('');
+  });
+
+  $("#remove-address").click(function() {
+
+    var addressNumber = $(".one-address");
+    console.log(addressNumber.length);
+    if(addressNumber.length !== 1) {
+      $('.one-address').first().remove();
+    }
+  });
 
 
 });
